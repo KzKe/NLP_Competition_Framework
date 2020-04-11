a0a10a8e7b62b09fb903e96e2fe53b91d9e1893c
KzKe <kangk1@student.unimelb.edu.au>
2020-04-11 19:14:37 +0800

    added Chinese bert path

--- Config.py
+++ Config.py
@@ -5,6 +5,13 @@ MAX_LEN = 512
 TRAIN_BATCH_SIZE = 8
 VALID_BATCH_SIZE = 4
 EPOCHS = 10
+
+# RoBERTa-wwm-ext-large	hfl/chinese-roberta-wwm-ext-large
+# RoBERTa-wwm-ext	hfl/chinese-roberta-wwm-ext
+# BERT-wwm-ext	hfl/chinese-bert-wwm-ext
+# BERT_PATH = "hfl/chinese-bert-wwm-ext"
+# BERT_PATH = "hfl/chinese-roberta-wwm-ext"
+# BERT_PATH = "hfl/chinese-roberta-wwm-ext-large"
 BERT_PATH = "../input/bert_base_uncased/"
 MODEL_PATH = "model.bin"
 TRAINING_FILE = "../input/imdb.csv"
